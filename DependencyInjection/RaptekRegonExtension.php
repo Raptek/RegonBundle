dd670a5bd09df0085d60ea63c1f3bb8c0aeab10e
Krzysztof Winiarski <raptilek@gmail.com>
2015-03-01 14:45:26 +0100

    Added basic configuration

--- DependencyInjection/RaptekRegonExtension.php
+++ DependencyInjection/RaptekRegonExtension.php
@@ -22,6 +22,8 @@ class RaptekRegonExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
+        $container->setParameter('raptek.regon.api_key', $config['api_key']);
+
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
